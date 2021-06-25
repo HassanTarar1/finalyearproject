70bc2905c256ac997c22917720fa479fb51d9d60
Hassan <hassanijaz0301@gmail.com>
2021-06-25 14:56:41 +0500

    go

---
+++ src/app/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+import Buttn from "@material-ui/core/Button"
+import Signup from "../component/signup"
+import Signin from "../component/signin"
+import { Form, Input, Button, Checkbox } from 'antd';
+import Registerf from "../component/registrationform"
+ 
+function App() {
+  
+ 
+  return (
+  <>
+<Signup/>
+<Signin/>
+    
+</>
+  );
+  
+}
+
+
+export default App;
